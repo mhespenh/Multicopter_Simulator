8ae165a2d5a5b344d24b974a620ec048976f677e
Michael Hespenheide <mhespenh@vt.edu>
2012-03-21 21:05:28 -0400

    Personalization

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-//Initial commit
-// Added mhespenh
-// Added rcole
-// Added ebarlow
-// All users merged
+/***********************************************************************
+  Group 3 ECE4574 Spring 2012
+  Project 3 - Multicopter Simulator
+    Multicopter_Simulator - Control process for simulator (mhespenh branch)
+
+  Revisions:
+    03/21/2012 - Initial version (master branch)
+    03/21/2012 - Initial version (mhespenh branch)
+***********************************************************************/
+
 #include <QtCore/QCoreApplication>
 
 int main(int argc, char *argv[])
