ead958e3b93bc5377cfb1051035cb7fcbdc36c07
Michael Hespenheide <mhespenh@vt.edu>
2012-03-21 21:07:23 -0400

    Okay, we're ready to go!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 /***********************************************************************
   Group 3 ECE4574 Spring 2012
   Project 3 - Multicopter Simulator
-    Multicopter_Simulator - Control process for simulator (mhespenh branch)
+    Multicopter_Simulator - Control process for simulator (master branch)
 
   Revisions:
     03/21/2012 - Initial version (master branch)
-    03/21/2012 - Initial version (mhespenh branch)
 ***********************************************************************/
 
 #include <QtCore/QCoreApplication>
