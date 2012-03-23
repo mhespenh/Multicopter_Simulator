3e3889545a80a452daf61f48d6bdc9409471b109
Michael Hespenheide <mhespenh@vt.edu>
2012-03-23 10:54:06 -0400

    Experimental build to test dBus connections

--- main.cpp
+++ main.cpp
@@ -10,12 +10,33 @@
 #include <QtCore/QCoreApplication>
 #include <MulticopterSimulator.h>
 #include <QDebug>
+#include <QtDBus/QtDBus>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    if (!QDBusConnection::sessionBus().isConnected()) {
+         fprintf(stderr, "Cannot connect to the D-Bus session bus.\n"
+                 "To start it, run:\n"
+                 "\teval `dbus-launch --auto-syntax`\n");
+         return 1;
+     }
 
-    MulticopterSimulator sim;
-    sim.writeData("Hello from another process\n");
-    return a.exec();
+    QDBusInterface iface("edu.vt.ece.simmotor", "/", "", QDBusConnection::sessionBus());
+     if (iface.isValid()) {
+         QDBusReply<QString> reply = iface.call("recvMessage", "Hello", 45);
+         if (reply.isValid()) {
+             printf("Reply was: %s\n", qPrintable(reply.value()));
+             return 0;
+         }
+
+         fprintf(stderr, "Call failed: %s\n", qPrintable(reply.error().message()));
+         return 1;
+     }
+
+     fprintf(stderr, "%s\n", qPrintable(QDBusConnection::sessionBus().lastError().message()));
+ //   MulticopterSimulator sim;
+ //   sim.writeData("Hello from another process\n");
+ //   return a.exec();
+     return 1;
 }
